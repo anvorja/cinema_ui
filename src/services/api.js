c1298d1e1e5a3d7475b321989ab5a435f5332de1
anvorja <borja.carlos@correounivalle.edu.co>
2025-08-06 03:10:32 -0500

    Add dockerfile nginx.conf and setup for .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 
-const API_BASE_URL = 'http://localhost:8080/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL ||'http://localhost:8080/api';
 // Crear instancia de axios
 const api = axios.create({
   baseURL: API_BASE_URL,
